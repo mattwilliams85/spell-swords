bfb123d8a4cf4b312a343c25c204b56300b434bf
mattwilliams85 <mattwilliams85@gmail.com>
2016-11-15 11:31:41 -0800

    adds tile event bindings

--- app/components/board/Board.js
+++ app/components/board/Board.js
@@ -2,29 +2,94 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import BoardButtons from './Buttons'
 import { bindActionCreators } from 'redux'
-import { letters } from '../../letters'
+import { randomAtoZ } from '../../letters'
 
 class Board extends Component {
-  render () {
+  constructor (props) {
+    super(props)
+    this.handleMouseUp = this.handleMouseUp.bind(this)
 
+    this.state = {hoverEvents: false, tiles: [], word: [], activeTiles: {}}
+  }
+
+  componentWillMount () {
     let tiles = []
-    for (var x = 0; x < 7; x++) {
-      for (var y = 0; y < 7; y++) {
-        let rand = Math.ceil(Math.random() * 25)
-        let letter = letters[rand]
-        tiles.push({x: x, y: y, character: letter.character, score: letter.score})
+    for (var x = 0; x < 6; x++) {
+      for (var y = 0; y < 6; y++) {
+        let letter = randomAtoZ()
+        tiles.push({
+          x: x,
+          y: y,
+          character: letter.character,
+          score: letter.score,
+          key: `x${x}-y${y}`
+        })
       }
     }
+    this.setState({tiles: tiles})
+  }
 
+  componentDidMount () {
+    window.addEventListener('mouseup', this.handleMouseUp)
+  }
+
+  handleMouseDown (tile) {
+    let activeTiles = this.addToActiveTiles(tile.key)
+    let word = this.addToWord(tile.character, tile.key)
+
+    this.setState({word: word, hoverEvents: true, activeTiles: activeTiles})
+  }
+
+  handleMouseUp () {
+    this.setState({hoverEvents: false, word: [], activeTiles: {}})
+  }
+
+  handleMouseOver (tile) {
+    if (!this.state.hoverEvents) return
+    if (this.state.activeTiles[tile.key]) return
+
+    let activeTiles = this.addToActiveTiles(tile.key)
+    let word = this.addToWord(tile.character, tile.key)
+
+    this.setState({word: word, activeTiles: activeTiles})
+  }
+
+  addToActiveTiles (key) {
+    let activeTile = {}
+    activeTile[key] = true
+    return Object.assign({}, this.state.activeTiles, activeTile)
+  }
+
+  addToWord (character, key) {
+    let newLetter = {character: character, key: key}
+    newLetter[key] = true
+    return [...this.state.word, newLetter]
+  }
+
+  isActiveTile (key) {
+    if (this.state.activeTiles[key]) return 'active'
+  }
+
+  render () {
     return (
       <div className='board-wrap'>
+        <div className='temp-word'>
+          { this.state.word.map(letter => {
+            return (<span key={letter.key}>{letter.character}</span>)
+          })}
+        </div>
         <div className='background layer-0' />
         <div className='tile-wrap'>
-          { tiles.map(tile => {
+          { this.state.tiles.map(tile => {
             return (
               <div
-                className={`x${tile.x} y${tile.y} tile`}
-                key={`x${tile.x}y${tile.y}`}>
+                className={`x${tile.x} y${tile.y} ${this.isActiveTile(tile.key)} tile`}
+                key={tile.key}
+                onMouseDown={this.handleMouseDown.bind(this, tile)}>
+                <div
+                  className='hover-box'
+                  onMouseOver={this.handleMouseOver.bind(this, tile)}
+                  value={tile.character} />
                 <span className='character'>{tile.character}</span>
                 <span className='score'>{tile.score}</span>
               </div>
--- app/letters.js
+++ app/letters.js
@@ -1,4 +1,43 @@
-export const letters = [
+export const randomAtoZ = ((lookup) => {
+  return function () {
+    let random = Math.random() * 100000
+    let letter
+    for (letter in lookup) {
+      if (random < lookup[letter]) {
+        return letters[letter]
+      }
+    }
+  }
+})({
+  0: 8167,
+  1: 9659,
+  2: 12441,
+  3: 16694,
+  4: 29396,
+  5: 31624,
+  6: 33639,
+  7: 39733,
+  8: 46699,
+  9: 46852,
+  10: 47624,
+  11: 51649,
+  12: 54055,
+  13: 60804,
+  14: 68311,
+  15: 70240,
+  16: 70335,
+  17: 76322,
+  18: 82649,
+  19: 91705,
+  20: 94463,
+  21: 95441,
+  22: 97801,
+  23: 97951,
+  24: 99925,
+  25: 100000
+})
+
+const letters = [
   {character: 'a', score: 1},
   {character: 'b', score: 3},
   {character: 'c', score: 3},
@@ -26,4 +65,3 @@ export const letters = [
   {character: 'y', score: 4},
   {character: 'z', score: 10}
 ]
-
